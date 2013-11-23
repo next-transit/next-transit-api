31b9739ee32f8747cfdcb94cc3ae70f3cd26682d
Reed Lauber <reedlauber@gmail.com>
2013-11-23 11:27:17 -0700

    Added bbox filter for stops resource.

--- server/lib/controllers/stops.js
+++ server/lib/controllers/stops.js
@@ -4,14 +4,24 @@ var ctrl = require('./controller').create('stops', true),
 ctrl.action('index', function(req, res, success) {
 	var route_id = (req.query.route_id || req.params.route_id || '').toLowerCase(),
 		direction_id = (req.query.direction_id || req.params.direction_id || ''),
-		min_sequence = (req.query.min_sequence || '');
+		min_sequence = (req.query.min_sequence || ''),
+		bbox = '', left = 0, bottom = 0, right = 0, top = 0;
+
+	if(req.query.bbox) {
+		bbox = req.query.bbox.split(',');
+		left = parseFloat(bbox[0]);
+		bottom = parseFloat(bbox[1]);
+		right = parseFloat(bbox[2]);
+		top = parseFloat(bbox[3]);	
+	}
 
 	stops
-		.error(req.internal_error)
 		.where('agency_id = ?', [req.agency.id])
-		.orders('stop_sequence')
+		.where_if('stop_lon > ? AND stop_lon < ? AND stop_lat > ? AND stop_lat < ?', [left, right, bottom, top], bbox)
+		.orders('stop_name')
 		.limit(ctrl.limit)
 		.count(true)
+		.error(req.internal_error)
 		.done(function(results, count) {
 			success({
 				stops: stops.public(results),
