4018bcb4fe49efdc5911168aa989c237b72c691c
Reed Lauber <reedlauber@gmail.com>
2013-12-17 12:43:44 -0700

    Added stop_times import transform for > 24hr values.

--- server/lib/importer/transforms.js
+++ server/lib/importer/transforms.js
@@ -10,6 +10,28 @@ transforms.trips = function(record) {
 	record.block_id = record.block_id || 0;
 };
 
+transforms.stop_times = function(record) {
+	var time_parts, hr;
+
+	if(record.arrival_time) {
+		time_parts = record.arrival_time.split(':');
+		hr = parseInt(time_parts, 10);
+		if(hr <= 3) {
+			time_parts[0] = hr + 24;
+			record.arrival_time = time_parts.join(':');
+		}
+	}
+	
+	if(record.departure_time) {
+		time_parts = record.departure_time.split(':');
+		hr = parseInt(time_parts, 10);
+		if(hr <= 3) {
+			time_parts[0] = hr + 24;
+			record.departure_time = time_parts.join(':');
+		}
+	}
+};
+
 module.exports = {
 	get_transform: function(type) {
 		return transforms[type] || noop
