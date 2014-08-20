64069eef3e050598a90bb1531a1784328a1ad210
Reed Lauber <reedlauber@gmail.com>
2014-08-19 22:06:38 -0700

    Only show shapes and stops within bounds
    To speed up map bounds with a lot of routes. Much less data to process,
    move and render.

--- server/lib/controllers/shapes.js
+++ server/lib/controllers/shapes.js
@@ -15,12 +15,19 @@ function get_route_by_multi_id(agency_id, route_id) {
 	});
 }
 
-function get_simplified_stops_by_route(agency_id, route, include_stops) {
+function get_simplified_stops_by_route(agency_id, route, bbox, include_stops) {
 	return new promise(function(resolve, reject) {
 		if(!include_stops) {
 			resolve([]);
 		} else {
-			simplified_stops.where('agency_id = ? AND route_id = ?', [agency_id, route.route_id])
+			var bbox_array = [];
+			if(bbox) {
+				bbox_array = [bbox.west, bbox.south, bbox.east, bbox.north];
+			}
+
+			simplified_stops.query()
+				.where('agency_id = ? AND route_id = ?', [agency_id, route.route_id])
+				.where_if('stop_lon > ? AND stop_lat > ? AND stop_lon < ? AND stop_lat < ?', bbox_array, bbox)
 				.orders('direction_id, stop_sequence')
 				.error(reject)
 				.done(function(stops) {
@@ -34,9 +41,16 @@ function get_simplified_stops_by_route(agency_id, route, include_stops) {
 	});
 }
 
-function get_simplified_shape_by_route(agency_id, route) {
+function get_simplified_shape_by_route(agency_id, route, bbox) {
 	return new promise(function(resolve, reject) {
-		simplified_shapes.where('agency_id = ? AND route_id = ?', [agency_id, route.route_id])
+		var bbox_array = [];
+		if(bbox) {
+			bbox_array = [bbox.west, bbox.south, bbox.east, bbox.north];
+		}
+
+		simplified_shapes.query()
+			.where('agency_id = ? AND route_id = ?', [agency_id, route.route_id])
+			.where_if('shape_pt_lon > ? AND shape_pt_lat > ? AND shape_pt_lon < ? AND shape_pt_lat < ?', bbox_array, bbox)
 			.orders('segment_id, id')
 			.done(function(points) {
 				var segments = {};
@@ -60,13 +74,13 @@ function get_simplified_shape_by_route(agency_id, route) {
 	});
 }
 
-function get_paths_and_stops_for_route(agency_id, route_id, include_stops) {
+function get_paths_and_stops_for_route(agency_id, route_id, bbox, include_stops) {
 	return new promise(function(resolve, reject) {
 		get_route_by_multi_id(agency_id, route_id).then(function(route) {
 			if(route) {
 				promise.all([
-					get_simplified_shape_by_route(agency_id, route),
-					get_simplified_stops_by_route(agency_id, route, include_stops)
+					get_simplified_shape_by_route(agency_id, route, bbox),
+					get_simplified_stops_by_route(agency_id, route, bbox, include_stops)
 				]).done(function(results) {
 					route.paths = results[0];
 					route.stops = results[1];
@@ -79,7 +93,7 @@ function get_paths_and_stops_for_route(agency_id, route_id, include_stops) {
 	});
 }
 
-function get_shapes_for_routes(agency_id, route_results, include_stops) {
+function get_shapes_for_routes(agency_id, route_results, bbox, include_stops) {
 	return new promise(function(resolve, reject) {
 		if(!route_results || !route_results.length) {
 			resolve([]);
@@ -91,12 +105,37 @@ function get_shapes_for_routes(agency_id, route_results, include_stops) {
 
 		route_results.forEach(function(result) {
 			if(result.route_id) {
-				promises.push(get_paths_and_stops_for_route(agency_id, result.route_id, include_stops));
+				promises.push(get_paths_and_stops_for_route(agency_id, result.route_id, bbox, include_stops));
 			}
 		});
 
 		promise.all(promises).done(resolve);
 	});
+}
+
+function get_buffered_bbox(bbox, percent) {
+	var decimal_percent = percent * 0.01,
+		x_size = bbox.east - bbox.west, // east - west
+		y_size = bbox.north - bbox.south, // north - south
+		x_buffer = x_size * decimal_percent,
+		y_buffer = y_size * decimal_percent;
+
+	return {
+		west: bbox.west - x_buffer,
+		south: bbox.south - y_buffer,
+		east: bbox.east + x_buffer,
+		north: bbox.north + y_buffer
+	};
+}
+
+function string_to_bbox(bbox_str) {
+	var bbox_split = bbox_str.split(','),
+		left = parseFloat(bbox_split[0]),
+		bottom = parseFloat(bbox_split[1]),
+		right = parseFloat(bbox_split[2]),
+		top = parseFloat(bbox_split[3]);
+
+	return { west:left, south:bottom, east:right, north:top };
 }
 
 ctrl.action('index', { json:true }, function(req, res, callback) {
@@ -106,8 +145,8 @@ ctrl.action('index', { json:true }, function(req, res, callback) {
 		get_route_by_multi_id(req.agency.id, route_id).then(function(route) {
 			if(route) {
 				promise.all([
-					get_simplified_shape_by_route(req.agency.id, route),
-					get_simplified_stops_by_route(req.agency.id, route, true)
+					get_simplified_shape_by_route(req.agency.id, route, null),
+					get_simplified_stops_by_route(req.agency.id, route, null, true)
 				]).done(function(results) {
 					route.paths = results[0];
 					route.stops = results[1];
@@ -124,21 +163,21 @@ ctrl.action('index', { json:true }, function(req, res, callback) {
 
 ctrl.action('bbox', { json:true }, function(req, res, callback) {
 	if(req.query.bbox) {
-		var bbox = req.query.bbox.split(','),
-			left = parseFloat(bbox[0]),
-			bottom = parseFloat(bbox[1]),
-			right = parseFloat(bbox[2]),
-			top = parseFloat(bbox[3]),
+		var bbox = string_to_bbox(req.query.bbox),
+			bbox_array = [bbox.west, bbox.south, bbox.east, bbox.north],
+			buffered_bbox = get_buffered_bbox(bbox, 20),
 			include_stops = req.query.stops !== 'false';
 
 		simplified_shapes.query()
 			.select('distinct route_id')
-			.where('agency_id = ? AND shape_pt_lon > ? AND shape_pt_lon < ? AND shape_pt_lat > ? AND shape_pt_lat < ?', [req.agency.id, left, right, bottom, top])
+			.where('agency_id = ?', [req.agency.id])
+			.where_if('shape_pt_lon > ? AND shape_pt_lat > ? AND shape_pt_lon < ? AND shape_pt_lat < ?', bbox_array, true)
 			.limit(ctrl.limit)
 			.count(true)
 			.done(function(results, count) {
-				get_shapes_for_routes(req.agency.id, results, include_stops).then(function(routes) {
+				get_shapes_for_routes(req.agency.id, results, buffered_bbox, include_stops).then(function(routes) {
 					callback({
+						bbox: buffered_bbox,
 						data: routes,
 						count: routes.count,
 						total_count: count
