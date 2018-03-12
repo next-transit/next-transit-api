ace73a989464cdac9904a0febf31dc12f97b117b
Reed Lauber <reedlauber@gmail.com>
2018-03-12 14:59:24 -0700

    More map and trips support

---
+++ server/lib/realtime/request.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const promise = require('promise');
+
+function request(url) {
+  return new promise(function(resolve, reject) {
+    http.get(url, function(res) {
+      res.setEncoding('utf8');
+
+      var data = '';
+      res.on('data', function(chunk) {
+        data += chunk;
+      }).on('end', function() {
+        var data_obj = null;
+        try {
+          data_obj = JSON.parse(data);
+        } catch(e) {
+          return reject('Could not parse realtime response from ' + url);
+        }
+        
+        resolve(data_obj);
+      });
+    }).on('error', function(err) {
+      reject(err);
+    });
+  });
+}
+
+module.exports = request;
---
+++ server/lib/realtime/trimet.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const querystring = require('querystring');
+const promise = require('promise');
+const config = require('../util/config');
+const route_types = require('../util/route_types');
+const request = require('./request');
+
+const trimet = {};
+const REALTIME_URL = 'http://developer.trimet.org/beta/v2/vehicles';
+
+function get_request_url(routeId) {
+  const params = {
+    appID: '606DD5685EEC20DACC9E720B4',
+    routes: routeId
+  };
+
+  return REALTIME_URL + '?' + querystring.stringify(params);
+}
+
+function normalize(routeTypeId, data) {
+  const vehicles = [];
+
+  if(data && data.resultSet && data.resultSet.vehicle) {
+    data.resultSet.vehicle.forEach((datum) => {
+      vehicles.push({
+        mode: route_types.getMode(routeTypeId),
+        lat: datum.latitude,
+        lng: datum.longitude,
+        vehicle_id: datum.vehicleID.toString(),
+        block_id: datum.blockID,
+        destination: datum.signMessageLong,
+        direction: datum.direction,
+        offset: null,
+        late: 0 - Math.floor(datum.delay / 60)
+      });
+    });
+  }
+
+  return vehicles;
+}
+
+trimet.get_vehicles = function(routeTypeId, routeId) {
+  return new promise(function(resolve, reject) {
+    request(get_request_url(routeId)).then(function(data) {
+      resolve(normalize(routeTypeId, data));
+    }, reject)
+  });
+};
+
+module.exports = trimet;
