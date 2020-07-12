f61a346fb4a11032983dbb597e2df8490bf8a41a
Reed Lauber <reedlauber@gmail.com>
2020-07-12 15:41:26 -0700

    Temporarily disable realtime vehicles

--- server/lib/controllers/vehicles.js
+++ server/lib/controllers/vehicles.js
@@ -3,18 +3,20 @@ var realtime_ctrls = require('../realtime');
 
 ctrl.action('index', { json:true }, function(req, res, callback) {
   try {
-    var agency_realtime = realtime_ctrls[req.agency.slug];
+    // var agency_realtime = realtime_ctrls[req.agency.slug];
 
-    const agencyId = req.agency.id;
-    var { route_type, route_id } = req.route.params;
+    // const agencyId = req.agency.id;
+    // var { route_type, route_id } = req.params;
 
-    if (!route_type || !route_id) {
-      return res.error('Vehicles request requires a route ID');
-    }
+    // if (!route_type || !route_id) {
+    //   return res.error('Vehicles request requires a route ID');
+    // }
 
-    agency_realtime.get_vehicles(agencyId, route_type, route_id).then((vehicles) => {
-      callback({ vehicles:vehicles });
-    }, res.error);
+    // agency_realtime.get_vehicles(agencyId, route_type, route_id).then((vehicles) => {
+    //   callback({ vehicles:vehicles });
+    // }, res.error);
+
+    callback({ vehicles:[] });
   } catch(e) {
     res.internal_error('Couldn\'t load agency realtime library.');
     console.error(e);
